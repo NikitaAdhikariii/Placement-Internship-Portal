8226a405c8f44b2707d003e8f8df4c77b5959867
Nikita Adhikari <your-github-email@example.com>
2026-04-26 14:36:57 +0530

    Fixed gitignore and removed node_modules

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,29 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-
-  name: {
-    type: String,
-    required: true
-  },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-
-  password: {
-    type: String,
-    required: true
-  },
-
-  role: {
-    type: String,
-    enum: ["student", "company", "admin"],
-    required: true
-  }
-
-}, { timestamps: true });
+  name: String,
+  email: String,
+  password: String,
+  role: String,
+  cgpa: Number,
+  skills: [String],
+  resume: String
+});
 
 module.exports = mongoose.model("User", userSchema);
